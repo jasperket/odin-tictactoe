2d18a0f7be9c38c251c694058a986734dca9ebd8
jasperket <jasperkeith1.vallecera@gmail.com>
2023-10-05 21:24:56 +0800

    Added switching players when a mark is added

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-const Gameboard = (() => {
-    let marks = 0;
-    const board = [
-        ['','',''],
-        ['','',''],
-        ['','','']
-    ];
-    const markAdded = () => marks++;
-    return {board, markAdded};
-})();
-
 const Player = (mark) => {
     const addMark = (row,column) => {
         Gameboard.board[row][column] = mark;
         Gameboard.markAdded();
     }
-    return {addMark};
+    const getMark = () => {return mark};
+    return {addMark, getMark};
 }
 
 const playerOne = Player('X');
 const playerTwo = Player('O');
+
+const Gameboard = (() => {
+    let marks = 0;
+    let gameEnd = false;
+    let playerTurn = playerOne;
+    const board = [
+        ['','',''],
+        ['','',''],
+        ['','','']
+    ];
+    const boardButtons = document.querySelectorAll('.board button');
+    boardButtons.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            const row = btn.dataset.row;
+            const col = btn.dataset.col;
+            playerTurn.addMark(row,col);
+            btn.textContent = playerTurn.getMark();
+            if (playerTurn === playerOne) {
+                playerTurn = playerTwo;
+            } else {
+                playerTurn = playerOne;
+            }
+        })
+    });
+    const markAdded = () => marks++;
+    return {board, markAdded};
+})();
